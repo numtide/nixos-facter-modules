26560ce9e5975d818b2cd3c3b17bc6831e4e1d74
Brian McGee <brian@bmcgee.ie>
2024-08-16 16:30:02 +0100

    feat: recursively find unit tests

--- flake.nix
+++ flake.nix
@@ -24,5 +24,29 @@
         "riscv64-linux"
         "x86_64-linux"
       ];
-    };
+    }
+    // (
+      let
+        inherit (inputs.nixpkgs) lib;
+
+        findTests =
+          path:
+          with lib;
+          filterAttrsRecursive (_n: v: v != { }) (
+            mapAttrs' (
+              n: v:
+              if (v == "regular" && (hasSuffix ".unit.nix" n)) then
+                nameValuePair n (import "${path}/${n}" lib)
+              else if v == "directory" then
+                nameValuePair n (findTests "${path}/${n}")
+              else
+                nameValuePair n { }
+            ) (builtins.readDir path)
+          );
+      in
+      {
+        # you can run all the tests from a devshell with `nix-unit --flake .#unit-tests` or `nix build .#unit-tests`
+        unit-tests = findTests ./.;
+      }
+    );
 }
--- packages/unit-tests.nix
+++ packages/unit-tests.nix
@@ -5,11 +5,25 @@
   inputs,
   ...
 }:
+let
+  inherit (pkgs) lib;
+  overrideInputs =
+    with lib;
+    concatStringsSep " " (
+      mapAttrsToList (n: v: "--override-input ${n} ${v}") (filterAttrs (n: _: n != "self") inputs)
+    );
+in
 pkgs.runCommandLocal pname { nativeBuildInputs = [ pkgs.nix-unit ]; } ''
   set -euo pipefail
   export HOME="$(realpath .)"
-  for test in $(find ${flake} -type f -name "*.unit.nix"); do
-    nix-unit --eval-store $HOME -I nixpkgs=${inputs.nixpkgs} $test
-  done
+
+  # nix-unit must be able to find all used inputs in the store, otherwise it will try to download them
+  # this is why we provide the `--override-input` entries below
+
+  nix-unit --eval-store "$HOME" \
+    --extra-experimental-features flakes \
+    ${overrideInputs} \
+    --flake ${flake}#unit-tests
+
   touch $out
 ''
