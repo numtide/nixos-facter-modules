5d1fa0ee9ccc740343f23d3f34ba9dfe68cec7f8
Johannes Kirschbauer <hsjobeki@gmail.com>
2024-08-27 15:52:16 +0200

    Remove all dependencies

--- flake.nix
+++ flake.nix
@@ -3,50 +3,37 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    systems.url = "github:nix-systems/default";
-    blueprint = {
-      url = "github:numtide/blueprint";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.systems.follows = "systems";
-    };
-    treefmt-nix = {
-      url = "github:numtide/treefmt-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs =
-    inputs:
-    inputs.blueprint {
-      inherit inputs;
-      systems = [
-        "aarch64-linux"
-        "riscv64-linux"
-        "x86_64-linux"
-      ];
-    }
-    // (
-      let
-        inherit (inputs.nixpkgs) lib;
+  outputs = inputs: let
+    inherit (inputs.nixpkgs) lib;
 
-        findTests =
-          path:
-          with lib;
-          filterAttrsRecursive (_n: v: v != { }) (
-            mapAttrs' (
-              n: v:
-              if (v == "regular" && (hasSuffix ".unit.nix" n)) then
-                nameValuePair n (import "${path}/${n}" lib)
-              else if v == "directory" then
-                nameValuePair n (findTests "${path}/${n}")
-              else
-                nameValuePair n { }
-            ) (builtins.readDir path)
-          );
-      in
-      {
-        # you can run all the tests from a devshell with `nix-unit --flake .#unit-tests` or `nix build .#unit-tests`
-        unit-tests = findTests ./.;
-      }
-    );
+    findTests = path:
+      with lib;
+        filterAttrsRecursive (_n: v: v != {}) (
+          mapAttrs' (
+            n: v:
+              if (v == "regular" && (hasSuffix ".unit.nix" n))
+              then nameValuePair n (import "${path}/${n}" lib)
+              else if v == "directory"
+              then nameValuePair n (findTests "${path}/${n}")
+              else nameValuePair n {}
+          ) (builtins.readDir path)
+        );
+  in {
+    lib = import ./lib { inherit inputs; };
+
+    nixosConfigurations = {
+      basic = (import ./hosts/basic { inherit inputs; flake = inputs.self; }).value;
+    };
+    nixosModules = {
+      boot = ./modules/nixos/boot.nix;
+      facter = ./modules/nixos/facter.nix;
+      firmware = ./modules/nixos/firmware.nix;
+      networking = ./modules/nixos/networking;
+      system = ./modules/nixos/system.nix;
+      virtualisation = ./modules/nixos/virtualisation.nix;
+    };
+    unit-tests = findTests ./.;
+  };
 }
