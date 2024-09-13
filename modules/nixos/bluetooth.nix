832359f4a5fa6e22ea6e150f55b8a7fe53536710
Jörg Thalheim <joerg@thalheim.io>
2024-09-13 16:25:25 +0200

    bluetooth: simplify and fix incorrect factor.boot condition

--- modules/nixos/bluetooth.nix
+++ modules/nixos/bluetooth.nix
@@ -1,21 +1,8 @@
 { lib, config, ... }:
-let
-
-  cfg = config.facter.boot;
-  inherit (config.facter) report;
-in
 {
   options.facter.bluetooth.enable = lib.mkEnableOption "Enable the Facter bluetooth module" // {
-    default =
-      let
-        bluetooth = report.hardware.bluetooth or [ ];
-      in
-      builtins.length bluetooth > 0;
+    default = builtins.length (config.facter.report.hardware.bluetooth or []) > 0;
   };
 
-  config =
-    with lib;
-    mkIf cfg.enable {
-      hardware.bluetooth.enable = lib.mkIf config.facter.bluetooth.enable (lib.mkDefault true);
-    };
+  config.hardware.bluetooth.enable = lib.mkIf config.facter.bluetooth.enable (lib.mkDefault true);
 }
