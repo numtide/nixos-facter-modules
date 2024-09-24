2c3ac40acc87cb4f7c2b61c421e0622343a10b24
Jörg Thalheim <joerg@thalheim.io>
2024-09-24 16:40:10 +0200

    get rid of lib.uniq

--- modules/nixos/boot.nix
+++ modules/nixos/boot.nix
@@ -3,6 +3,8 @@ let
 
   cfg = config.facter.boot;
   inherit (config.facter) report;
+  collectDriver = list: lib.foldl' (lst: value: lst ++ value.driver_modules or [ ]) [ ] list;
+  stringSet = list: builtins.attrNames (builtins.groupBy lib.id list);
 in
 {
   options.facter.boot.enable = lib.mkEnableOption "Enable the Facter Boot module" // {
@@ -12,25 +14,15 @@ in
   config =
     with lib;
     mkIf cfg.enable {
-      boot.initrd.availableKernelModules = filter (dm: dm != null) (
-        unique (
-          map
-            (
-              {
-                driver_module ? null,
-                ...
-              }:
-              driver_module
-            )
-            (flatten [
-              # Needed if we want to use the keyboard when things go wrong in the initrd.
-              (report.hardware.usb_controller or [ ])
-              # A disk might be attached.
-              (report.hardware.firewire_controller or [ ])
-              # definitely important
-              (report.hardware.disk or [ ])
-              (report.hardware.storage_controller or [ ])
-            ])
+      boot.initrd.availableKernelModules = stringSet (
+        collectDriver (
+          # Needed if we want to use the keyboard when things go wrong in the initrd.
+          (report.hardware.usb_controller or [ ])
+          # A disk might be attached.
+          ++ (report.hardware.firewire_controller or [ ])
+          # definitely important
+          ++ (report.hardware.disk or [ ])
+          ++ (report.hardware.storage_controller or [ ])
         )
       );
     };
