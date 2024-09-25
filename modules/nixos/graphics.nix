7bc4cd41c33bc11e60f565de643df96dba59641e
Jörg Thalheim <joerg@thalheim.io>
2024-09-25 15:24:54 +0200

    graphics: exclude noveau kernel module

--- modules/nixos/graphics.nix
+++ modules/nixos/graphics.nix
@@ -1,16 +1,28 @@
 { lib, config, ... }:
 let
   facterLib = import ../../lib/lib.nix lib;
+  cfg = config.facter.detected.graphics;
 in
 {
-  options.facter.detected.graphics.enable = lib.mkEnableOption "Enable the Graphics module" // {
-    default = builtins.length (config.facter.report.hardware.monitor or [ ]) > 0;
+  options.facter.detected = {
+    graphics.enable = lib.mkEnableOption "Enable the Graphics module" // {
+      default = builtins.length (config.facter.report.hardware.monitor or [ ]) > 0;
+    };
+    boot.graphics.kernelModules = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      # We currently don't auto import nouveau, in case the user might want to use the proprietary nvidia driver,
+      # We might want to change this in future, if we have a better idea, how to handle this.
+      default = lib.remove "nouveau" (
+        facterLib.stringSet (facterLib.collectDrivers (config.facter.report.hardware.graphics_card or [ ]))
+      );
+      description = ''
+        List of kernel modules to load at boot for the graphics card.
+      '';
+    };
   };
 
-  config = lib.mkIf config.facter.detected.graphics.enable {
+  config = lib.mkIf cfg.enable {
     hardware.graphics.enable = lib.mkDefault true;
-    boot.initrd.kernelModules = facterLib.stringSet (
-      facterLib.collectDrivers (config.facter.report.hardware.graphics_card or [ ])
-    );
+    boot.initrd.kernelModules = config.facter.detected.boot.graphics.kernelModules;
   };
 }
