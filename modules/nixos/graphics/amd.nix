d2f40f8e13290a8bb4a116e94a46f2b8857486b3
Benedikt Ritter <beneritter@gmail.com>
2024-10-27 12:23:49 +0100

    graphics (amd): Make accessing graphics card drivers more resilient
    For empty facter reports it's possible that neighter hardware nor
    graphics_card is set.

--- modules/nixos/graphics/amd.nix
+++ modules/nixos/graphics/amd.nix
@@ -1,14 +1,20 @@
-{lib, config,...}:
+{ lib, config, ... }:
 let
   cfg = config.facter.detected.graphics.amd;
 in
 {
   options.facter.detected.graphics = {
     amd.enable = lib.mkEnableOption "Enable the AMD Graphics module" // {
-      default = builtins.elem "amdgpu" (builtins.map (graphics_card: graphics_card.driver) config.facter.report.hardware.graphics_card);
+      default =
+        let
+          graphicsDrivers = builtins.map (graphics_card: graphics_card.driver) (
+            config.facter.report.hardware.graphics_card or [ ]
+          );
+        in
+        builtins.elem "amdgpu" graphicsDrivers;
     };
   };
   config = lib.mkIf cfg.enable {
-  services.xserver.videoDrivers = ["amdgpu"];
+    services.xserver.videoDrivers = [ "amdgpu" ];
   };
 }
