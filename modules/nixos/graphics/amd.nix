a1d95f4fbb52236f9a4f7f56514e648a11800f97
Benedikt Ritter <beneritter@gmail.com>
2024-10-27 12:35:10 +0100

    graphics (amd): Use facter lib to access drivers

--- modules/nixos/graphics/amd.nix
+++ modules/nixos/graphics/amd.nix
@@ -1,17 +1,14 @@
 { lib, config, ... }:
 let
+  facterLib = import ../../../lib/lib.nix lib;
   cfg = config.facter.detected.graphics.amd;
 in
 {
   options.facter.detected.graphics = {
     amd.enable = lib.mkEnableOption "Enable the AMD Graphics module" // {
-      default =
-        let
-          graphicsDrivers = builtins.map (graphics_card: graphics_card.driver) (
-            config.facter.report.hardware.graphics_card or [ ]
-          );
-        in
-        builtins.elem "amdgpu" graphicsDrivers;
+      default = builtins.elem "amdgpu" (
+        facterLib.collectDrivers (config.facter.report.hardware.graphics_card or [ ])
+      );
     };
   };
   config = lib.mkIf cfg.enable {
