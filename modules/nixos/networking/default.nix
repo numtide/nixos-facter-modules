cbbb0f780bddc5648ca4ee33a6c788e07c971776
Benedikt Ritter <beneritter@gmail.com>
2024-11-12 14:04:52 +0100

    networking: fix incorrect report path for network_interfaces

--- modules/nixos/networking/default.nix
+++ modules/nixos/networking/default.nix
@@ -6,7 +6,7 @@
   ];
 
   options.facter.detected.dhcp.enable = lib.mkEnableOption "Facter dhcp module" // {
-    default = builtins.length config.facter.report.network_interface or [ ] > 0;
+    default = builtins.length config.facter.report.hardware.network_interface or [ ] > 0;
   };
   config = lib.mkIf config.facter.detected.dhcp.enable {
     networking.useDHCP = lib.mkDefault true;
